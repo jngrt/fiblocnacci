fca6677148ae725e3e4cbab846e23f14c0353bbb
jngrt <jangeertmunneke@gmail.com>
2017-10-19 00:09:13 +0200

    checking the grid with iterators

--- src/GridModel.js
+++ src/GridModel.js
@@ -35,52 +35,116 @@ export default class GridModel{
     //return a copy of the array
     return this.grid.map( row => row.slice());
   }
-  findSequences(row, col){
-    console.log('findSequences');
-    // loop through all rows
-    this.sequences = [];
-    let r,c;
-    this.current = [];
-    let val, last;
 
-    //for(r = 0; r < this.size; r++ ){
-    r = 1;
-      for( c = 0; c < this.size; c++ ){
-        val = this.grid[r][c];
+  // startRow, endRow, rowMod, startCol, endCol, colMod
+  findSequences(){
+    this.sequences = [];
+    this.findRowRight();
+    this.findRowLeft();
+    this.findColDown();
+    this.findColUp();
+
+    console.log(this.sequences);
+    //this.sequences.forEach( console.log );
+  }
+
+  findRowRight(){
+    let r = 0;
+    let c = 0;
+    this.findSeqIter( {
+      nextLine: () => {
+        c = 0;
+        return ++r < this.size
+      },
+      nextCell: () => ++c < this.size,
+      getVal: () => this.grid[r][c],
+      getData: () => ({r, c, val: this.grid[r][c]}),
+      toString: () => `${r} - ${c}`
+    });
+  }
+  findRowLeft(){
+    let r = this.size - 1;
+    let c = this.size - 1;
+    this.findSeqIter( {
+      nextLine: () => {
+        c = this.size - 1;
+        return --r >= 0
+      },
+      nextCell: () => --c >= 0,
+      getVal: () => this.grid[r][c],
+      getData: () => ({r, c, val: this.grid[r][c]}),
+      toString: () => `${r} - ${c}`
+    });
+  }
+  findColDown(){
+    let r = 0;
+    let c = 0;
+    this.findSeqIter( {
+      nextLine: () => {
+        r = 0;
+        return ++c < this.size
+      },
+      nextCell: () => ++r < this.size,
+      getVal: () => this.grid[r][c],
+      getData: () => ({r, c, val: this.grid[r][c]}),
+      toString: () => `${r} - ${c}`
+    });
+  }
+  findColUp(){
+    let r = this.size - 1;
+    let c = this.size - 1;
+    this.findSeqIter( {
+      nextLine: () => {
+        r = this.size - 1;
+        return --c >= 0
+      },
+      nextCell: () => --r >= 0,
+      getVal: () => this.grid[r][c],
+      getData: () => ({r, c, val: this.grid[r][c]}),
+      toString: () => `${r} - ${c}`
+    });
+  }
+
+  findSeqIter(iter){
+    //TODO, rewrite with ES6 iterators
+
+    let line, cell;
+
+    while( line = iter.nextLine() ) {
+      //console.log(iter.toString());
+      this.current = [];
+
+      while( cell = iter.nextCell() ) {
+
+        let val = iter.getVal();
 
         // end the sequence if it's not a FN
         if( !val || !this.isFibonacci( val ) ){
-          this.endOfSequence();
+          this.endSequence();
 
         // in case it is a FN
         } else  {
 
-          // if no current seq, or beginning of row: start new seq.
-          if( !this.current.length || !c ) {
-            this.current.push({r,c,val});
-
           // if this is next in the sequence: save it
-          } else if ( this.isFollowUp( last, val ) ) {
-            this.current.push({r,c,val});
+          if ( this.fitInSequence( val ) ) {
+            this.current.push(iter.getData());
 
           // if FN, but not in seq: start new sequence
           } else {
-            this.endOfSequence();
+            this.endSequence();
             // start a new sequence with the value just found
-            this.current = [{r,c,val}];
+            this.current = [iter.getData()];
           }
         }
-        last = val;
       }
       //check if seq at end of row...
-      this.endOfSequence();
-
-    //}
-    console.log(this.sequences);
-    this.sequences.forEach( console.log );
+      this.endSequence();
+    }
   }
 
-  endOfSequence() {
+
+
+  endSequence() {
     if( !this.current.length ) return;
 
     this.filterOnes(this.current);
@@ -92,8 +156,10 @@ export default class GridModel{
     this.current = [];
   }
 
+  // there can be an arbitrary number of ones at the start, reduce to two
   filterOnes(ar){
 
+    // if the first value is not one, there's nothing to be done
     if( ar[0].val !== 1 ) return;
 
     // array.findLastIndex doesn't exist, therefore we reverse it.
@@ -110,11 +176,19 @@ export default class GridModel{
     return ar.reverse();
   }
 
-  isFollowUp(x,y) {
-    if ( x === 1 && y ===1 )
+  // check if a number fits in the sequence
+  fitInSequence(val) {
+    //first one in sequence always fits
+    if( !this.current.length )
       return true;
 
-    return this.memory[ this.memory.lastIndexOf(x) + 1] === y;
+    //check if previous one and new one are 1's
+    let last = this.current[ this.current.length - 1].val;
+    if ( val === 1 && last ===1 )
+      return true;
+
+    // otherwise check if it fits in the sequence
+    return this.memory[ this.memory.lastIndexOf(last) + 1] === val;
   }
 
   // check if a certain number is fibonacci
@@ -124,6 +198,7 @@ export default class GridModel{
     // check if the number is in the fibonacci cache
     return this.memory.indexOf(n) !== -1;
   }
+
   // "lazy" generating fibonacci numbers, only when we encounter bigger numbers
   generateFibonacci(max) {
     if(!this.memory){
@@ -134,4 +209,49 @@ export default class GridModel{
       mem.push( mem[mem.length - 1] + mem[mem.length -2] );
     }
   }
+
+  /*
+  // The old findsequences, purely for historical reasons
+  findSequences(row, col){
+    console.log('findSequences');
+    // loop through all rows
+    this.sequences = [];
+    this.current = [];
+
+    let val;
+    let r,c;
+    for(r = 0; r < this.size; r++ ){
+      for( c = 0; c < this.size; c++ ){
+        val = this.grid[r][c];
+
+        // end the sequence if it's not a FN
+        if( !val || !this.isFibonacci( val ) ){
+          this.endSequence();
+
+        // in case it is a FN
+        } else  {
+
+          // if no current seq, or beginning of row: start new seq.
+          if( !this.current.length || !c ) {
+            this.current.push({r,c,val});
+
+          // if this is next in the sequence: save it
+          } else if ( this.fitInSequence( val ) ) {
+            this.current.push({r,c,val});
+
+          // if FN, but not in seq: start new sequence
+          } else {
+            this.endSequence();
+            // start a new sequence with the value just found
+            this.current = [{r,c,val}];
+          }
+        }
+      }
+      //check if seq at end of row...
+      this.endSequence();
+
+    }
+    console.log(this.sequences);
+    this.sequences.forEach( console.log );
+  }*/
 }
